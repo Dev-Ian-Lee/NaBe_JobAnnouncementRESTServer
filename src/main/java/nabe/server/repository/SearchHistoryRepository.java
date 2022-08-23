c635fe3cb84b5ca8d7bf575df18d6ee0bd932fc7
desfox <desfox99@naver.com>
2022-08-23 22:18:27 +0900

    FEAT: mssql 대신 mysql 사용하기 위해 코드 수정

--- src/main/java/nabe/server/repository/SearchHistoryRepository.java
+++ src/main/java/nabe/server/repository/SearchHistoryRepository.java
@@ -27,31 +27,31 @@ public class SearchHistoryRepository {
     public Map<String, List> findMostUsedSearchConditions() {
 
         // 기업명, 공고명, 업직종 키워드, 직무내용에서 검색하고자 하는 검색어
-        List<KeyWordsMapping> keywords = em.createNativeQuery("SELECT keywords FROM search_history GROUP BY(keywords) HAVING COUNT(*) = (SELECT TOP(1) COUNT(*) FROM search_history GROUP BY(keywords) ORDER BY COUNT(*) DESC)")
+        List<KeyWordsMapping> keywords = em.createNativeQuery("SELECT keywords FROM search_history GROUP BY(keywords) HAVING COUNT(*) = (SELECT COUNT(*) FROM search_history GROUP BY(keywords) ORDER BY COUNT(*) DESC LIMIT 1)")
                 .getResultList();
 
         searchConditions.put("keywords", keywords);
 
         // 근무지/지역 조건
-        List<LocCodeMapping> loc_cd = em.createNativeQuery("SELECT loc_cd FROM search_history GROUP BY(loc_cd) HAVING COUNT(*) = (SELECT TOP(1) COUNT(*) FROM search_history GROUP BY(loc_cd) ORDER BY COUNT(*) DESC)")
+        List<LocCodeMapping> loc_cd = em.createNativeQuery("SELECT loc_cd FROM search_history GROUP BY(loc_cd) HAVING COUNT(*) = (SELECT COUNT(*) FROM search_history GROUP BY(loc_cd) ORDER BY COUNT(*) DESC LIMIT 1)")
                 .getResultList();
 
         searchConditions.put("loc_cd", loc_cd);
 
         // 직무코드
-        List<JobCodeMapping> job_cd = em.createNativeQuery("SELECT job_cd FROM search_history GROUP BY(job_cd) HAVING COUNT(*) = (SELECT TOP(1) COUNT(*) FROM search_history GROUP BY(job_cd) ORDER BY COUNT(*) DESC)")
+        List<JobCodeMapping> job_cd = em.createNativeQuery("SELECT job_cd FROM search_history GROUP BY(job_cd) HAVING COUNT(*) = (SELECT COUNT(*) FROM search_history GROUP BY(job_cd) ORDER BY COUNT(*) DESC LIMIT 1)")
                 .getResultList();
 
         searchConditions.put("job_cd", job_cd);
 
         // 근무형태/고용형태 조건
-        List<JobTypeMapping> job_type = em.createNativeQuery("SELECT job_type FROM search_history GROUP BY(job_type) HAVING COUNT(*) = (SELECT TOP(1) COUNT(*) FROM search_history GROUP BY(job_type) ORDER BY COUNT(*) DESC)")
+        List<JobTypeMapping> job_type = em.createNativeQuery("SELECT job_type FROM search_history GROUP BY(job_type) HAVING COUNT(*) = (SELECT COUNT(*) FROM search_history GROUP BY(job_type) ORDER BY COUNT(*) DESC LIMIT 1)")
                 .getResultList();
 
         searchConditions.put("job_type", job_type);
 
         // 학력조건
-        List<EduLvMapping> edu_lv = em.createNativeQuery("SELECT edu_lv FROM search_history GROUP BY(edu_lv) HAVING COUNT(*) = (SELECT TOP(1) COUNT(*) FROM search_history GROUP BY(edu_lv) ORDER BY COUNT(*) DESC)")
+        List<EduLvMapping> edu_lv = em.createNativeQuery("SELECT edu_lv FROM search_history GROUP BY(edu_lv) HAVING COUNT(*) = (SELECT COUNT(*) FROM search_history GROUP BY(edu_lv) ORDER BY COUNT(*) DESC LIMIT 1)")
                 .getResultList();
 
         searchConditions.put("edu_lv", edu_lv);
