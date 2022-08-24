ee4f239cf0333761c06bcdcda2df43933d6c6948
desfox <desfox99@naver.com>
2022-08-24 23:43:02 +0900

    FEAT: local mysql 서버 대신 rds 사용 위해 암호화 툴 jasypt 사용(작업 중)

--- src/main/java/nabe/server/ServerApplication.java
+++ src/main/java/nabe/server/ServerApplication.java
@@ -1,11 +1,13 @@
 package nabe.server;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+@EnableEncryptableProperties
 @SpringBootApplication
 public class ServerApplication {
 
--- src/test/java/nabe/server/DbTest.java
+++ src/test/java/nabe/server/DbTest.java
@@ -2,10 +2,8 @@ package nabe.server;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
